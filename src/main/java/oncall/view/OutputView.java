a22248b4104c17c43b3b0686682ff30ebfb14197
dongheeHan <ark182818@gmail.com>
2026-01-03 20:17:08 +0900

    fix: 평일이면서 공휴일인 경우 출력 후 return 추가

--- src/main/java/oncall/view/OutputView.java
+++ src/main/java/oncall/view/OutputView.java
@@ -11,22 +11,23 @@ public class OutputView {
 
     public void printWorkSchedule(OncallDate date, Workers workers) {
         int workingMonth = date.getMonth();
-        while (true) {
-            if (date.getMonth() != workingMonth) break;
-
+        while (date.getMonth() == workingMonth) {
             printWorkScheduleLine(date, workers);
             date = date.plusOneDay();
         }
     }
 
     private void printWorkScheduleLine(OncallDate date, Workers workers) {
-        if (date.isWeekend() && date.isHoliday()) {
-            System.out.printf(WORK_SCHEDULE_HOLIDAY_FORMAT,
-                    date.getMonth(), date.getDay(), date.getDayOfWeekFirstString(), workers.work(date));
-        }
-        System.out.printf(WORK_SCHEDULE_WEEKDAY_FORMAT,
-                date.getMonth(), date.getDay(), date.getDayOfWeekFirstString(), workers.work(date));
+        String worker = workers.work(date);
 
+        if (date.isWeekday() && date.isHoliday()) {
+            System.out.printf(WORK_SCHEDULE_HOLIDAY_FORMAT,
+                    date.getMonth(), date.getDay(), date.getDayOfWeekFirstString(), worker);
+            return;
+        }
+
+        System.out.printf(WORK_SCHEDULE_WEEKDAY_FORMAT,
+                date.getMonth(), date.getDay(), date.getDayOfWeekFirstString(), worker);
     }
 
     public void printErrorMessage(String message) {
